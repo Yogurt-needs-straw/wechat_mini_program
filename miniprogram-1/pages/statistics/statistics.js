40ef4a14e25819c4edd4b9cf1c680d74549c3d93
养乐多要吸管 <a102467@outlook.com>
2023-09-20 20:35:45 +0800

    Mini Program Data Statistics Page

--- miniprogram-1/pages/statistics/statistics.js
+++ miniprogram-1/pages/statistics/statistics.js
@@ -1,4 +1,7 @@
 // pages/statistics/statistics.js
+
+const api = require("../../config/api.js")
+
 Page({
 
   /**
@@ -8,11 +11,27 @@ Page({
     dataList:[]
   },
 
+  getRecord:function(){
+    wx.showLoading({mask:true})
+    wx.request({
+      url: api.bankStatistics,
+      method:"GET",
+      success :(res) =>{
+        this.setData({
+          dataList:res.data
+        })
+      },
+      complete:()=>{
+        wx.hideLoading()
+      }
+    })
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getRecord();
   },
 
   /**
@@ -47,7 +66,7 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    this.getRecord();
   },
 
   /**
