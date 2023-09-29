279caa5927881f135c73b8b48cdf756d3ba45b32
养乐多要吸管 <a102467@outlook.com>
2023-09-29 16:42:54 +0800

    Local storage of user login information

--- miniprogram-1/app.js
+++ miniprogram-1/app.js
@@ -1,8 +1,26 @@
 // app.js
 App({
   onLaunch() {
+    // storage 获取本都存储 登录
+    var userInfo = wx.getStorageSync('userInfo');
+    if(userInfo){
+      this.globalData.userInfo = userInfo;
+    }
   },
   globalData: {
-
+    userInfo:null
+  },
+  initUserInfo: function(name,userId,avatar) {
+    var info = {
+      name: name,
+      userId: userId,
+      avatar: avatar
+    };
+    this.globalData.userInfo = info
+    wx.setStorageSync('userInfo', info);
+  },
+  logoutUserInfo:function(){
+    wx.removeStorageSync('userInfo');
+    this.globalData.userInfo=null;
   }
 })
---
+++ miniprogram-1/pages/activity/activity.js
@@ -0,0 +1,93 @@
+// pages/activity/activity.js
+var app = getApp()
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    activityList:[
+      {
+        "id": 1,
+        "disabled": true,
+        "title": "x1",
+        "text": "123...",
+        "date": "2023-09-27",
+        "count": 0,
+        "score": 10
+    }
+    ]
+  },
+
+  doApply(nid){
+    // 1.判断是否已登录(vuex)
+    var userInfo = app.globalData.userInfo;
+    
+    // 2.未登录，则跳转到登录页面
+    if(!userInfo){
+      wx.switchTab({
+        url:'/pages/mine/mine',
+      })
+      return
+    }
+
+    // 3.已登录，继续请求
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
